3b2e82b8b805c5b2933231a02f15395bd7139788
veronakva <88837874+veronakva@users.noreply.github.com>
2021-08-17 10:07:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+var a= 35;
+var b = 'Hello ';
+var c = true; 
+var d = 5;
+
+alert('Добрый день');
+
+prompt ('write the number for a)')
+
+if (a == 35 ) {
+	alert('Vse verno');
+ {
+ else {
+ alert ('else')
+	}
+
+for var = 0;
+	i<5;
+	i ++
+	){
+alert (i);
+
+var user = {
+	'name': 'Петя',
+	'surname': 'Васичкин',
+	'age': 25
+}
+ alert (user ['name'])
+ alert (user.age)
+var mas = [ 'Петя', 'Васичкин', 25];
+alert (mas[0])
+
+function doMagic (){
+	aalert(5+5)
+}
+
+
+var btn = document.getElementbyId ('btn');
+btn.onclick= function (e) {
+e.preventDefault ();
+var text = document.querySelector ('.intro')
+	text.classlist.add ('red')
+}
+var img = document.querySelector ('.desktop');
+img.style.display = 'none';
+document.querySelector ('.skill-change') .style.marginLeft= 
